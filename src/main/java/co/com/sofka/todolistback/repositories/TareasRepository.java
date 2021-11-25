1be13dac6489734592a6f0deabb8ac9fe40cdc3c
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-25 00:16:45 -0500

    creando repositorio

--- src/main/java/co/com/sofka/todolistback/repositories/TareasRepository.java
+++ src/main/java/co/com/sofka/todolistback/repositories/TareasRepository.java
@@ -1,4 +1,9 @@
 package co.com.sofka.todolistback.repositories;
 
-public class TareasRepository {
+import co.com.sofka.todolistback.models.TareasModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TareasRepository extends CrudRepository<TareasModel, Long> {
 }

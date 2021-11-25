7b9452265020fadf4b177ca7faa40fbecde2bd96
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-25 00:47:09 -0500

    Finalizando el back-end

--- src/main/java/co/com/sofka/todolistback/controllers/TareasController.java
+++ src/main/java/co/com/sofka/todolistback/controllers/TareasController.java
@@ -1,4 +1,41 @@
 package co.com.sofka.todolistback.controllers;
 
+import co.com.sofka.todolistback.models.TareasModel;
+import co.com.sofka.todolistback.services.TareaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins ="http://localhost:3000")
 public class TareasController {
+    @Autowired
+    private TareaService service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<TareasModel> list() {
+        return service.list();
+    }
+
+    @PostMapping(value = "api/todo")
+    public TareasModel save(@RequestBody TareasModel tarea) {
+        return service.save(tarea);
+    }
+
+    @PutMapping(value = "api/todo")
+    public TareasModel update(@RequestBody TareasModel tarea) {
+        if (tarea.getId() != null) {
+            return service.save(tarea);
+        }
+        throw new RuntimeException("No existe el ID");
+    }
+
+    @GetMapping(value = "api/{id}/todo")
+    public TareasModel get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id") Long id) {
+        service.delete(id);
+    }
 }
